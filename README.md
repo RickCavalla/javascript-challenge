a0c28d7450a058f85e685c27669d089f270071c1
Rick Cavalla <monolith32@gmail.com>
2019-10-07 20:52:51 -0400

    Final with Three filters

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# This is a placeholder
+# Basic filter by date, with blank treated as all dates
+# Bonus dropdown of shapes for filtering
+# Bonus fill-in for case-insensitive string search of comments
--- static/js/app.js
+++ static/js/app.js
@@ -2,21 +2,31 @@
 var ufoData = data;
 
 // YOUR CODE HERE!
+/* Grab handle of filter button */
 var ufo_button = d3.select("#filter-btn");
 
+/* Handles to filter options */
+var ufo_date = d3.select("#filter-date");
 var ufo_select = d3.select("#filter-shape");
+var ufo_contains = d3.select("#filter-contains");
 
+/* Build an array of the unique shapes in data object */
 var ufoShapes = data.map(ufo => ufo.shape);
 var shapeSet = new Set(ufoShapes);
 var uniqueShapes = Array.from(shapeSet);
 
-console.log(uniqueShapes);
-
+/* Add an "ALL" option for shapes as default */
 var select_item = ufo_select.append("option");
-
 select_item.attr("value", "ALL");
 select_item.text("ALL");
 
+/* Convert text dates in data to date variables */
+ufoData.forEach((ufo) => {
+    var dateSplit = ufo.datetime.split("/");
+    ufo.datetime = new Date(dateSplit[2], dateSplit[0] - 1, dateSplit[1]);
+});
+
+/* for each uniquie shape in array, add to select options */
 uniqueShapes.forEach((ufoShape) => {
     console.log(ufoShape);
     var select_item = ufo_select.append("option");
@@ -25,35 +35,51 @@ uniqueShapes.forEach((ufoShape) => {
     select_item.text(ufoShape);
 });
 
+/* Click the filter button */
 ufo_button.on("click", function() {
+    var dateValue = ufo_date.property("value");
+    var shapeValue = ufo_select.property("value");
+    var containsValue = ufo_contains.property("value").toLowerCase();
 
-    // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#ufo-datetime");
+    /* start with all data */
+    filteredData = ufoData;
   
-    // Get the value property of the input element
-    var inputValue = inputElement.property("value");
-  
-    console.log(inputValue);
-
-    var filteredData = ufoData.filter(ufo => ufo.datetime === inputValue);
-
-    console.log(filteredData);
-    console.log(ufo_select.property("value"));
-
-    if (ufo_select.property("value") != "ALL") {
-        filteredData = filteredData.filter(ufo => ufo.shape === ufo_select.property("value"))
+    /* Only filter date if non-blank.  Blank will indicate all dates */
+    if (dateValue != "") {
+        var dateSplit = dateValue.split("/");
+        var dateValue = new Date(dateSplit[2], dateSplit[0] - 1, dateSplit[1]);
+        /* Dates in javascript are touchy about equality if you don't convert to
+         * number by putting a plus sign in front */
+        var filteredData = filteredData.filter(ufo => +ufo.datetime === +dateValue);
     }
 
-    console.log(filteredData);
+    /* Only filter if shape selected is not ALL */
+    if (shapeValue != "ALL") {
+        filteredData = filteredData.filter(ufo => ufo.shape === shapeValue)
+    }
 
+    /* Only do string search if search value is not blank */
+    if (containsValue != "") {
+        filteredData = filteredData.filter(ufo => ufo.comments.toLowerCase().search(containsValue) >= 0)
+    }
+
+    /* select tbody */
     var ufo_tbody = d3.select("tbody");
+
+    /* empty out tbody so we can build new table */
     ufo_tbody.html("");
 
+    /* Go through each ufo report that we have filtered and display as a row */
     filteredData.forEach((ufoReport) => {
         var ufo_tr = ufo_tbody.append("tr");
 
         var ufo_td = ufo_tr.append("td");
-        ufo_td.text(ufoReport.datetime);
+        var dateSimple = 
+            (ufoReport.datetime.getMonth() + 1) + "/"
+            + ufoReport.datetime.getDate() + "/"
+            + ufoReport.datetime.getFullYear();
+
+        ufo_td.text(dateSimple);
 
         var ufo_td = ufo_tr.append("td");
         ufo_td.text(ufoReport.city);
